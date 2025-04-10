0c7fd6ed74d9f777ae223132be570337ce6d4a1b
SwastikMallik <mallikswastik@gmail.com>
2025-04-10 13:34:34 +0530

    Cors issue fixed

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,14 @@
 const express = require("express");
 
+const cors = require("cors");
+
 const app = express();
+
+
+
+app.use(express.json());
+
+app.use(cors({ origin: "http://localhost:5173" }));
 
 app.get("/", (req, res)=>{
     const newRes = {
