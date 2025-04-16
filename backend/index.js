2aa2290297180550293fbb35ead4c57cd5c5e83d
SwastikMallik <mallikswastik@gmail.com>
2025-04-16 18:17:50 +0530

    fixing api call res

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,9 @@ const cors = require("cors");
 
 const app = express();
 
+app.use(express.json());
+
+
 const mongoose = require('mongoose')
 
 mongoose.connect('mongodb+srv://swastik25:Mongodb2520@no-sql-learning.wfhnsdu.mongodb.net/?retryWrites=true&w=majority&appName=no-sql-learning')
@@ -13,9 +16,10 @@ mongoose.connect('mongodb+srv://swastik25:Mongodb2520@no-sql-learning.wfhnsdu.mo
 
 const productModel = require("./models/Product")
 
-app.use(express.json());
 
-app.use(cors({ origin: "http://localhost:5173" }));
+
+//app.use(cors({ origin: "http://localhost:5173", allowedHeaders: ['Content-type', 'application-json'] }));
+app.use(cors({}));
 
 app.get("/", (req, res)=>{
     console.log(req)
@@ -29,13 +33,15 @@ app.get("/", (req, res)=>{
 
 app.post("/add-product", async(req, res)=>{
     console.log(req.body)
-    const result = await productModel.collection.insertOne(req.body)
-    const newRes = {
-        status : 200,
-        data : "Hello, Welcome to MegaFarm",
-        message : "No new message"
+    if(!req.body == undefined){
+        const result = await productModel.collection.insertOne(req.body)
+        const newRes = {
+            status : 200,
+            data : "Hello, Welcome to MegaFarm",
+            message : "No new message"
+        }
+        res.send(result)
     }
-    res.send(result)
 })
 
 app.listen(5001)
