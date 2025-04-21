5dc40c9d45b8d73a99d5f09782bd59a624286554
SwastikMallik <mallikswastik@gmail.com>
2025-04-21 23:20:03 +0530

    modal setup -1

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -15,6 +15,39 @@ const Home = () => {
         getAllProducts()
     },[])
 
+    //Edit Product detail
+    const openModal = function(id){
+        console.log(id, "Hello, Edit")
+    }
+
+    ///////
+    // Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+//////
+
     return (
         <>
             <h1>Home Page</h1>
@@ -26,10 +59,21 @@ const Home = () => {
                         <li>{item.category}</li>
                         <li>{item.quantity}</li>
                         <li>{item.price}</li>
-                        <li><a href="#">Edit</a></li>
+                        <li><a onClick={openModal.bind(null, item._id)}>Edit</a></li>
                     </ul>
                 )
             }
+            <button id="myBtn">Open Modal</button>
+
+
+<div id="myModal" className="modal">
+
+  <div className="modal-content">
+    <span className="close">&times;</span>
+    <p>Some text in the Modal..</p>
+  </div>
+
+</div>
         </>
     )
 }
