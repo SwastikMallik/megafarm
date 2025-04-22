ca873f1c126f6fbc852b661770c8ad515ccb1b98
SwastikMallik <mallikswastik@gmail.com>
2025-04-22 22:17:07 +0530

    Update data Modal box

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -3,6 +3,7 @@ import Header from "./Header"
 
 const Home = () => {
     const [product, setProduct] = useState([]);
+    const [selectionID, setSelectionID] = useState(null)
 
     const getAllProducts = async function(){
         const res = await fetch("http://localhost:5002/products")
@@ -16,37 +17,31 @@ const Home = () => {
     },[])
 
     //Edit Product detail
+
+    // Get the modal
+
+    const modal = document.getElementById("myModal");
+
     const openModal = function(id){
         console.log(id, "Hello, Edit")
+        modal.style.display = "block";
+        setSelectionID(id)
     }
 
-    ///////
-    // Get the modal
-var modal = document.getElementById("myModal");
+    const closeModal = function(){
+        modal.style.display = "none";
+    }
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+    window.onclick = function(event) {
+          if (event.target == modal) {
+            modal.style.display = "none";
+          }
+        }
+    
+    
+    
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-//////
+    
 
     return (
         <>
@@ -63,17 +58,18 @@ window.onclick = function(event) {
                     </ul>
                 )
             }
+            {selectionID}
+
             <button id="myBtn">Open Modal</button>
 
+            <div id="myModal" className="modal">
 
-<div id="myModal" className="modal">
+                <div className="modal-content">
+                    <span onClick={closeModal} className="close">&times;</span>
+                    <p className="blck">{selectionID}</p>
+                </div>
 
-  <div className="modal-content">
-    <span className="close">&times;</span>
-    <p>Some text in the Modal..</p>
-  </div>
-
-</div>
+            </div>
         </>
     )
 }
